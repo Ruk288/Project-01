2edbee98b09e008b8b7fa2bfbbf7603899587e28
operation badar <ob@gmail.com>
2016-07-22 11:21:40 -0800

    function calling
    function form another file

--- new2.py
+++ new2.py
@@ -7,5 +7,3 @@ def func_power(c,d):
     e=math.pow(c,d)
     print(e)
 
-func_mine(int(input("enter any number")))
-func_power(int(input("enetr base")), int(input("enter power")))
---
+++ random.py
@@ -0,0 +1,4 @@
+from new2 import func_mine, func_power
+
+print(func_mine(int(input("enter any number"))))
+func_power(int(input("enetr base")), int(input("enter power")))
