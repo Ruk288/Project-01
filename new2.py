3c9b1b4aafb041ebcb4f1d85ba880fc5543eae1a
operation badar <ob@gmail.com>
2016-07-22 10:31:47 -0800

    function made  by me
    function using built in function

---
+++ new2.py
@@ -0,0 +1,11 @@
+import math
+def func_mine(a):
+    b=abs(a)
+    print(b)
+
+def func_power(c,d):
+    e=math.pow(c,d)
+    print(e)
+
+func_mine(int(input("enter any number")))
+func_power(int(input("enetr base")), int(input("enter power")))
