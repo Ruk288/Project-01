895c043aff03d2f2327f26ee3e04ca8533ced925
Rukaiya Javaid <rjavaidsh@gmail.com>
2016-07-23 21:28:46 +0500

    Chapter 2 exercises
    All try yourself solved

--- concat.py
+++ concat.py
@@ -2,10 +2,30 @@ famous_person ="Albert Einstein"
 message= famous_person + " " + "once said, " + '"A person who never made a mistake never tried anything new"'
 print(message)
 
+# removing extra spaces
 name= "   \t Rukaiya    "
 print(name)
 print(name.strip())
 print(name.lstrip())
 print(name.rstrip())
 
+# using exponent
+a=3
+b=2
+c=(b**a)+2
+print(c)
 
+# printing integer as string
+age=23
+print("my age is " + str(age) + " years")
+
+# working on integers
+print(5+3)
+print(2*4)
+print(10-2)
+print(16/2)
+
+
+# printing my favourite number
+a=2
+print("my favourite number is " + str(a))
