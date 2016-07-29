bd19f41129d5a32433b50dfdf18cf013b071b9cf
Rukaiya Javaid <rjavaidsh@gmail.com>
2016-07-29 05:58:46 +0500

    Chapter 7
    Not all exercises

---
+++ Chapter#07.py
@@ -0,0 +1,20 @@
+# input function
+message=input("type message: ")
+print("my message is " +message)
+
+
+prompt="your message"
+prompt+="what is your name"
+name=input(prompt)
+print(name)
+
+# using int to accept numerical inputs
+
+age=input("yur age")
+print(int(age)+1)
+
+####### WHILE LOOPS
+current_num=1
+while current_num<=5:
+    print(current_num)
+    current_num +=1
